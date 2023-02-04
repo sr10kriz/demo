acc597dad1365fa517eb90a7f3e88cb228dc08d9
sridharakrishnan <sridharakrishnan@angleritech.com>
2023-02-04 11:45:46 +0530

    commit changes on sample

--- app/sample.js
+++ app/sample.js
@@ -6,7 +6,12 @@ console.log(person);
 // person = object | name,age = property
 person.name = "Portialla";
 // dot notation (this is better choice)
-console.log(person.name);
+person["name"] = "JoshPortialla";
+console.log(person["name"]);
+person["name"] = "JoshPortialla";
+console.log(person["name"]);
+person["name"] = "JoshPortialla";
+console.log(person["name"]);
 // OR bracket notation
 person["name"] = "JoshPortialla";
 console.log(person["name"]);
